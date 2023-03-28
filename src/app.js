4893ad658308052a2f028346c8004b4a7835effb
axeowl <benny.taccardi@gmail.com>
2023-03-28 09:04:33 +0200

    Feature: Integrated keycloak
    Integrated keycloak client and protected first APIs.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello Worldsss!');
+
+const keycloak = require('./config/keycloak-config.js').initKeycloak();
+app.use(keycloak.middleware());
+
+const testController = require('./controller/test-controller.js');
+app.use('/test', testController);
+
+app.get('/', function(req, res){
+   res.send("Server is up!");
 });
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+    console.log('Example app listening on port 3000!');
+  });
